a5a679a3fc566ee9f547b4d405004e3f9209e12a
Eric Normandeau - Bioinformatics <eric.normandeau.qc@gmail.com>
2012-08-14 15:03:11 -0400

    Improved "progress_bar.py"

--- progress_bar.py
+++ progress_bar.py
@@ -5,24 +5,24 @@ USAGE:
     %program total progress
 
 EXAMPLE:
-    progress_bar.py 30 18
+    progress_bar.py 30 18 [name of process]
 
 gives:
 
-<==================------------>
+process_name number_done: 3/30 (===---------------------------) 10.0%
 
 """
 import sys
 
 try:
-    total = int(sys.argv[1])
-    progress = int(sys.argv[2])
+    number_total = int(sys.argv[1])
+    number_done = int(sys.argv[2])
 except:
     print __doc__
     sys.exit(1)
 
-completed = str("%3.1f" % (100. * progress / total))
-#completed = (5 - len(completed)) * " " + completed
+percent_done = 100. * number_done / number_total
+#percent_done = str("%3.1f" % (100. * number_done / number_total))
 
-print "progress: " + str(progress) + "/" + str(total) + " (" + progress * "=" + (total - progress) * "-" + ") " + str(completed) + "%"
+print "number_done: " + str(number_done) + "/" + str(number_total) + " (" + int(percent_done * 0.3) * "=" + int((100 - percent_done) * 0.3) * "-" + ") " + str("%3.1f" % percent_done) + "%"
 
