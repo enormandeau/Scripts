30b785e5c578628aa47cf2130b18a14f2e89eec7
Eric Normandeau - Bioinformatics <eric.normandeau.qc@gmail.com>
2012-08-14 16:22:55 -0400

    Improved "progress_bar.py"

--- progress_bar.py
+++ progress_bar.py
@@ -9,7 +9,7 @@ EXAMPLE:
 
 gives:
 
-process_name number_done: 3/30 (===---------------------------) 10.0%
+process_name number_done: 3/30 (===                           ) 10.0%
 
 """
 import sys
@@ -29,5 +29,5 @@ except:
 percent_done = 100. * number_done / number_total
 #percent_done = str("%3.1f" % (100. * number_done / number_total))
 
-print process_name + ": " + str(number_done) + "/" + str(number_total) + " (" + int(percent_done * 0.3) * "=" + int((100 - percent_done) * 0.3) * "-" + ") " + str("%3.1f" % percent_done) + "%"
+print process_name + ": " + str(number_done) + "/" + str(number_total) + " (" + int(percent_done * 0.3) * "=" + int((100 - percent_done) * 0.3) * " " + ") " + str("%3.1f" % percent_done) + "%"
 
