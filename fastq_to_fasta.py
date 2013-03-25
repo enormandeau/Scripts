7c556d43e9757cca205eb3a254588edd8630abaa
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-03-25 15:39:00 -0400

    Added doc to fastq_to_fastq.py

--- fastq_to_fasta.py
+++ fastq_to_fasta.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+"""Convert fastq to fasta
+
+Usage:
+    python fastq_to_fastq.py inputFile outputFile
+"""
+
 import sys
 from Bio import SeqIO
-infile = sys.argv[1]
-outfile = sys.argv[2]
+try:
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+except:
+    print __doc__
+    sys.exit(1)
+
 SeqIO.write(SeqIO.parse(open(infile), "fastq"), open(outfile, "w"), "fasta")
