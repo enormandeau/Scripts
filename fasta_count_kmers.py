acbeceba1bc7e1f856e4a54b398024c11b929116
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-07-04 13:40:55 -0400

    fasta_count_kmers.py

---
+++ fasta_count_kmers.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+"""Return frequency of kmers present in a fasta file
+"""
+
+# Importing modules
+from signal import signal, SIGPIPE, SIG_DFL
+from collections import defaultdict
+import sys
+
+# Defining classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+# Defining functions
+def FastaIterator(object):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with open(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+        for line in f:
+            line = line.strip()
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+                name = line.replace(">", "")
+                sequence = ""
+                begun = True
+            else:
+                sequence += line
+        yield Fasta(name, sequence)
+
+
+if __name__ == '__main__':
+    # Prevent broken pipe error
+    signal(SIGPIPE, SIG_DFL)
+
+    # Parse user input
+    try:
+        input_file = sys.argv[1]
+        kmer_length = int(sys.argv[2])
+    except:
+        print __doc__
+        sys.exit(1)
+
+    # Cound kmers
+    kmer_dict = defaultdict(int)
+
+    for seq in FastaIterator(input_file):
+        for start in xrange(0, len(seq.sequence) - kmer_length):
+            stop = start + kmer_length
+            kmer_dict[seq.sequence[start: stop]] += 1
+
+    kmer_list = sorted([(kmer_dict[k], k) for k in kmer_dict], reverse=True)
+
+    # Print results
+    for kmer in kmer_list:
+        print kmer[1], kmer[0]
+
