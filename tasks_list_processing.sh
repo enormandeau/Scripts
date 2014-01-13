5a1d7d7a6511c5a39ea7256affddece3da3fb2a1
Eric Normandeau <eric.normandeau.qc@gmail.com>
2014-01-13 14:29:12 -0500

    Improved format

--- tasks_list_processing.sh
+++ tasks_list_processing.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 echo "PROCESSING"
 grep -R "^ +\.\." ~/Dropbox/Lab/project_checklists/*.txt | \
-    perl -pe 's/.*\///; s/\*/\t/; s/^/  /; s/\.txt://'
+    perl -pe 's/.*\///; s/\*/\t/; s/^/  /'
 echo
 
