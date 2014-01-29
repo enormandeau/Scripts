e3d964f9ab83d6b9f30e9f6315eab274cf1a4121
Eric Normandeau <eric.normandeau.qc@gmail.com>
2014-01-29 14:23:56 -0500

    Added send_weekly_cleaning_reminder.sh

---
+++ send_weekly_cleaning_reminder.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Script to send a reminder to those who have to clean the lab this week
+
+# Create list
+nb_people=$(wc -l ~/Documents/Menage/menage_liste.txt | cut -d " " -f 1)
+
+head -2 ~/Documents/Menage/menage_liste.txt \
+    > ~/Documents/Menage/menage_cette_semaine_temp.txt
+
+cat ~/Documents/Menage/menage_eric.txt \
+    ~/Documents/Menage/menage_cette_semaine_temp.txt \
+    >  ~/Documents/Menage/menage_cette_semaine.txt
+
+tail -$[ $nb_people - 2] ~/Documents/Menage/menage_liste.txt \
+    > ~/Documents/Menage/menage_temp.txt
+
+cat ~/Documents/Menage/menage_temp.txt \
+    ~/Documents/Menage/menage_cette_semaine_temp.txt \
+    > ~/Documents/Menage/menage_liste.txt
+
+cat ~/Documents/Menage/menage_message_rappel_francais.txt \
+    ~/Documents/Menage/menage_cette_semaine_temp.txt \
+    ~/Documents/Menage/menage_message_rappel_anglais.txt \
+    ~/Documents/Menage/menage_cette_semaine_temp.txt \
+    > ~/Documents/Menage/menage_message_rappel_cette_semaine.txt
+
+# Get password
+read -s -p "Email password: " account_password
+
+# Send reminder by email
+cat ~/Documents/Menage/menage_cette_semaine.txt | \
+    while read i
+    do
+        echo " -~=( Sending message to $i )=~-"
+        gmailsend.py \
+            eric.normandeau.qc@gmail.com \
+            $i \
+            "Rappel - Menage du labo" \
+            ~/Documents/Menage/menage_message_rappel_cette_semaine.txt \
+            $account_password
+    done
+
