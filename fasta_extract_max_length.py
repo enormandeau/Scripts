0ab9d9a00c89f7710716db0f5e9f148b6fda7ec5
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-06 09:54:03 -0400

    Remove Biopython use

--- fasta_extract_max_length.py
+++ fasta_extract_max_length.py
@@ -6,14 +6,45 @@
 Usage:
     %program <input_file> <output_file>"""
 
+import gzip
 import sys
 import re
 
-try:
-    from Bio import SeqIO
-except:
-    print "This program requires the Biopython library"
-    sys.exit(0)
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+# Functions
+def myopen(infile, mode="r"):
+    if infile.endswith(".gz"):
+        return gzip.open(infile, mode=mode)
+    else:
+        return open(infile, mode=mode)
+
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+        for line in f:
+            line = line.strip()
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+                name = line.replace(">", "")
+                sequence = ""
+                begun = True
+            else:
+                sequence += line
+        yield Fasta(name, sequence)
 
 try:
     fasta_file = sys.argv[1]      # Input fasta file
@@ -23,10 +54,10 @@ except:
     print __doc__
     sys.exit(0)
 
-fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+fasta_sequences = fasta_iterator(fasta_file)
 
-with open(result_file, "w") as f:
+with myopen(result_file, "w") as outfile:
     for seq in fasta_sequences:
-        if len(str(seq.seq)) <= max_length:
-            SeqIO.write([seq], f, "fasta")
+        if len(seq.sequence) <= max_length:
+            seq.write_to_file(outfile)
 
--- fastq_to_fasta.py
+++ fastq_to_fasta.py
@@ -5,8 +5,52 @@ Usage:
     python fastq_to_fastq.py inputFile outputFile
 """
 
+# Modules
 import sys
-from Bio import SeqIO
+
+# Defining classes
+class Fastq(object):
+    """Fastq object with name and sequence
+    """
+
+    def __init__(self, name, seq, name2, qual):
+        self.name = name[1:]
+        self.seq = seq
+        self.name2 = name2
+        self.qual = qual
+
+    def getShortname(self, separator):
+        self.temp = self.name.split(separator)
+        del(self.temp[-1])
+        return separator.join(self.temp)
+
+    def write_fasta(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.seq + "\n")
+
+# Defining functions
+def myopen(infile, mode="r"):
+    if infile.endswith(".gz"):
+        return gzip.open(infile, mode=mode)
+    else:
+        return open(infile, mode=mode)
+
+def fastq_parser(infile):
+    """Takes a fastq file infile and returns a fastq object iterator
+    """
+    
+    with myopen(infile) as f:
+        while True:
+            name = f.readline().strip()
+            if not name:
+                break
+
+            seq = f.readline().strip()
+            name2 = f.readline().strip()
+            qual = f.readline().strip()
+            yield Fastq(name, seq, name2, qual)
+
+# Parsing user input
 try:
     infile = sys.argv[1]
     outfile = sys.argv[2]
@@ -14,4 +58,10 @@ except:
     print __doc__
     sys.exit(1)
 
-SeqIO.write(SeqIO.parse(open(infile), "fastq"), open(outfile, "w"), "fasta")
+# Create sequence iterator
+sequences = fastq_parser(infile)
+
+# Treating the sequences
+with open(outfile, "w") as ofile:
+    for s in sequences:
+        s.write_fasta(ofile)
