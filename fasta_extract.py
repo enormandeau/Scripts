923b1618df67008df2e78f6e068e30e764754790
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-10-25 15:52:44 -0400

    Remove biopython depencency in fasta_extract.py

--- fasta_extract.py
+++ fasta_extract.py
@@ -8,14 +8,46 @@ Wanted file contains one sequence name per line.
 Usage:
     %program <input_file> <wanted_file> <output_file>"""
 
+import gzip
 import sys
 import re
 
-try:
-    from Bio import SeqIO
-except:
-    print "This program requires the Biopython library"
-    sys.exit(0)
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+# Functions
+def myopen(infile, mode="r"):
+    if infile.endswith(".gz"):
+        return gzip.open(infile, mode=mode)
+    else:
+        return open(infile, mode=mode)
+
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+        for line in f:
+            line = line.strip()
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+                name = line.replace(">", "")
+                sequence = ""
+                begun = True
+            else:
+                sequence += line
+        yield Fasta(name, sequence)
+
 
 try:
     fasta_file = sys.argv[1]  # Input fasta file
@@ -32,12 +64,12 @@ with open(wanted_file) as f:
         if line != "":
             wanted.add(line)
 
-fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+fasta_sequences = fasta_iterator(fasta_file)
 
 with open(result_file, "w") as f:
     for seq in fasta_sequences:
-        name = seq.id
-        if name in wanted and len(str(seq.seq)) > 0:
+        name = seq.name
+        if name in wanted and len(seq.sequence) > 0:
             wanted.remove(name) # Output only the first appearance for a name
-            SeqIO.write([seq], f, "fasta")
+            seq.write_to_file(f)
 
