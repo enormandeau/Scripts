536ae6cf4f30244783933d821c9e512d6bc922bc
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-12-12 21:11:31 -0500

    Deprecating lots of scripts

--- plot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gnuplot -p -e "set nokey; plot './$1'; pause -1"
--- update_my_scripts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo cp /home/labolb/Scripts/GitHub/Scripts/* /usr/local/bin
-sudo chmod 755 /usr/local/bin
-
