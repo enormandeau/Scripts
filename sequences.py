7dc35d5b9b45178faed14f3660b89ee8fc9b5c38
Eric Normandeau <eric.normandeau.qc@gmail.com>
2018-01-21 09:47:40 -0500

    Fasta and Fastq reading utilities

---
+++ sequences.py
@@ -0,0 +1,97 @@
+"""Module with Fasta and Fastq modules and file readers
+"""
+
+# Classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+    def __repr__(self):
+        return self.name + " " + self.sequence[:31]
+
+class Fastq(object):
+    """Fastq object with name, sequence, name2, and quality string
+    """
+
+    def __init__(self, name, sequence, name2, quality):
+        self.name = name
+        self.sequence = sequence
+        self.name2 = name2
+        self.quality = quality
+
+    def getShortname(self, separator):
+        if separator:
+            self.temp = self.name.split(separator)
+            del(self.temp[-1])
+            return separator.join(self.temp)
+
+        else:
+            return self.name
+
+    def write_to_file(self, handle):
+        handle.write(self.name + "\n")
+        handle.write(self.sequence + "\n")
+        handle.write(self.name2 + "\n")
+        handle.write(self.quality + "\n")
+
+    def __repr__(self):
+        return self.name + " " + self.sequence[:31]
+
+# Defining functions
+def myopen(_file, mode="r"):
+    if _file.endswith(".gz"):
+        return gzip.open(_file, mode=mode)
+
+    else:
+        return open(_file, mode=mode)
+
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+
+        for line in f:
+            line = line.strip()
+
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+
+                name = line[1:]
+                sequence = ""
+                begun = True
+
+            else:
+                sequence += line
+
+        if name != "":
+            yield Fasta(name, sequence)
+
+def fastq_iterator(infile):
+    """Takes a fastq file infile and returns a fastq object iterator
+
+    Requires fastq file with four lines per sequence and no blank lines.
+    """
+    
+    with myopen(infile) as f:
+        while True:
+            name = f.readline().strip()
+
+            if not name:
+                break
+
+            seq = f.readline().strip()
+            name2 = f.readline().strip()
+            qual = f.readline().strip()
+            yield Fastq(name, seq, name2, qual)
