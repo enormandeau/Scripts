a801184842ea66d1addc3c48175b4c4a4c65fafe
Eric Normandeau <eric.normandeau.qc@gmail.com>
2018-09-05 14:58:21 -0400

    Add fasta_sort_sequences_by_name.py

---
+++ fasta_sort_sequences_by_name.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""Sort sequences by name alpha-numerically
+
+Warning: Loads all the sequences in memory
+
+Usage:
+    <program> input_fasta output_fasta
+"""
+
+# Modules
+import sys
+
+# Classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+    def __repr__(self):
+        return self.name + " " + self.sequence[:31]
+
+# Defining functions
+def myopen(_file, mode="r"):
+    if _file.endswith(".gz"):
+        return gzip.open(_file, mode=mode)
+
+    else:
+        return open(_file, mode=mode)
+
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+
+        for line in f:
+            line = line.strip()
+
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+
+                name = line[1:]
+                sequence = ""
+                begun = True
+
+            else:
+                sequence += line
+
+        if name != "":
+            yield Fasta(name, sequence)
+
+# Parse user input
+try:
+    input_fasta = sys.argv[1]
+    output_fasta = sys.argv[2]
+except:
+    print(__doc__)
+    sys.exit(1)
+
+# Sort sequences
+sorted_sequences = sorted([(x.name, x.sequence) for x in list(fasta_iterator(input_fasta))])
+
+with open(output_fasta, "w") as outfile:
+    for s in sorted_sequences:
+        seq = Fasta(s[0], s[1])
+        seq.write_to_file(outfile)
