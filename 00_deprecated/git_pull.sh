ca0b597adddfcd273dd4900f58cf12de4095dc9b
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-12-19 09:59:45 -0500

    Delete old scripts

--- 00_deprecated/git_pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Update all git repositories in current folder
-
-find . -maxdepth 1 -type d | \
-    grep -vE '^.$' | \
-    parallel -j20 'echo -ne "## "{}"\n  " && cd {} && git pull'
-
