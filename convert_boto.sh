31cc2b5d1554fc20c5548e72b75acb3eade937aa
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-01-20 19:19:43 -0500

    Add convert_boto.sh

---
+++ convert_boto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FOLDER=$1
+
+IFS=$'\n'
+
+time for file in $(find $FOLDER -iname "*.jpg" | grep -v _small)
+do
+    echo $file
+    NEWNAME=$(echo $file | perl -pe 's/(\.jpg|\.JPG)/_small\1/')
+    convert -resize 800x800 -unsharp 1.0x1.0+1+0.04 $file $NEWNAME
+done
+
