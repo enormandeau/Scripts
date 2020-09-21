bab59381d08c37e232b1e0f4b36840e2760c6823
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-09-21 10:27:56 -0400

    Add fasta_lengths.py

---
+++ fasta_lengths.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""Print sequence lengths from fasta file
+
+Usage:
+    <program> input_fasta
+"""
+
+# Modules
+import gzip
+import sys
+
+# Classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+    def __repr__(self):
+        return self.name + " " + self.sequence[:31]
+
+# Defining functions
+def myopen(_file, mode="rt"):
+    if _file.endswith(".gz"):
+        return gzip.open(_file, mode=mode)
+
+    else:
+        return open(_file, mode=mode)
+
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = ""
+        name = ""
+        begun = False
+
+        for line in f:
+            line = line.strip()
+
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, sequence)
+
+                name = line[1:]
+                sequence = ""
+                begun = True
+
+            else:
+                sequence += line
+
+        if name != "":
+            yield Fasta(name, sequence)
+
+# Parse user input
+try:
+    input_fasta = sys.argv[1]
+except:
+    print(__doc__)
+    sys.exit(1)
+
+# Read fasta file
+sequences = fasta_iterator(input_fasta)
+
+for s in sequences:
+    print(len(s.sequence))
