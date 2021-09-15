22aa09fc29d93f7970039014acc09a8732a14670
Eric Normandeau <eric.normandeau.qc@gmail.com>
2021-09-15 08:52:47 -0400

    Remove Biopython dependency

--- fasta_min_len.py
+++ fasta_min_len.py
@@ -1,21 +1,71 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 """Display length of the shortest sequence of a fasta file.
 
 Usage:
-    %program file.fasta"""
+    <program> input_fasta
+"""
 
+# Modules
+import gzip
 import sys
 
-try:
-    from Bio import SeqIO
-except:
-    print "This program requires the Biopython library"
-    sys.exit(0)
+# Classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
 
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+    def __repr__(self):
+        return self.name + " " + self.sequence[:31]
+
+# Defining functions
+def myopen(_file, mode="rt"):
+    if _file.endswith(".gz"):
+        return gzip.open(_file, mode=mode)
+
+    else:
+        return open(_file, mode=mode)
+
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = []
+        name = ""
+        begun = False
+
+        for line in f:
+            line = line.strip()
+
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, "".join(sequence))
+
+                name = line[1:]
+                sequence = ""
+                begun = True
+
+            else:
+                sequence += line
+
+        if name != "":
+            yield Fasta(name, "".join(sequence))
+
+# Parsing user input
 try:
-    handle = open(sys.argv[1], 'rU')
-    print "Minimum sequence length:", min(map(lambda seq: len(seq.seq), \
-        SeqIO.parse(handle, 'fasta')))
+    input_fasta = sys.argv[1]
 except:
-    print __doc__
+    print(__doc__)
+    sys.exit(1)
+
+sequences = fasta_iterator(input_fasta)
+
+print(min([len(s.sequence) for s in sequences]))
