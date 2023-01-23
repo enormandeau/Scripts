2b92da1ca82705a96df3780579bc7c898b6eb201
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-01-23 13:26:31 -0500

    Improving progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -2,54 +2,45 @@
 """Print a progress bar given a total and a current progress
 
 USAGE:
-    %program total progress
+    %program completed total [process_name]
 
 EXAMPLE:
-    progress_bar.py 30 18 [name of process]
+    progress_bar.py 17 30 Testing
 
 gives:
-
-process_name number_done: 3/30 (===                           ) 10.0%
-
+    Test:           17/30   (=================             ) 56.7%
 """
 import sys
 import math
 
 try:
-    number_total = int(sys.argv[1])
-    number_done = int(sys.argv[2])
+    number_completed = float(sys.argv[1])
+    number_total = float(sys.argv[2])
 except:
     print(__doc__)
     sys.exit(1)
 
+assert number_completed <= number_total, "completed must be smaller or equal than total"
 try:
     process_name = sys.argv[3]
 except:
     process_name = "progress"
 
 try:
-    percent_done = 100. * number_done / number_total
+    percent_completed = 100. * number_completed / number_total
 except:
-    percent_done = 0
+    percent_completed = 0
 
 name = process_name + ": "
 name = name + (16 - len(name)) * " "
 
-numbers = str(number_done) + "/" + str(number_total) + " "
+numbers = str(number_completed) + "/" + str(number_total) + " "
 numbers = numbers + (8 - len(numbers)) * " "
 
-progress = int(math.floor(percent_done * 0.3)) * "="
+progress = int(math.floor(percent_completed * 0.3)) * "="
 progress = progress + (30 - len(progress)) * " "
 progress = "(" + progress + ") "
 
-percent = str("%3.1f" % percent_done) + "%"
+percent = str("%3.1f" % percent_completed) + "%"
 
 print(name + numbers + progress + percent)
-
-"""
-Processes:
-sai:        60/60    (==================================================) 100.0%
-sam:        60/60    (==================================================) 100.0%
-STACKS_fq1: 10/30    (================                                  ) 33.3%
-STACKS_fq1: 3/30     (=====                                             ) 10.0%
-"""
