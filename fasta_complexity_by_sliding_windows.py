31dce64b8949a54541eabc7cc3f3ed3f2c2aa9d0
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-04-05 14:28:31 -0400

    Add min_scaf_size

--- fasta_complexity_by_sliding_windows.py
+++ fasta_complexity_by_sliding_windows.py
@@ -2,10 +2,17 @@
 """Get sequence complexity by sliding-window for whole genomes
 
 Usage:
-    <program> input_genome window_size output_file
+    <program> input_genome window_size min_scaf_size output_file
+
+Where:
+    input_genome is a fasta or fasta.gz file with chromosomes or scaffolds
+    window_size is the size of the windows to evaluate complexity in
+    min_scaf_size is the minimum size of scaffold to be considered
+    output_file is the name of a file for the tabulation-separated output
 """
 
 # Modules
+from random import choice
 import gzip
 import sys
 
@@ -58,14 +65,31 @@ class Fasta(object):
     def __repr__(self):
         return self.name + " " + self.sequence[:31]
 
+def compression_ratio(seq, maximum_ratio=1.0):
+    return len(gzip.compress(seq.upper().encode())) / (len(seq) * maximum_ratio)
+
 # Parse user input
 try:
     input_file = sys.argv[1]
     window_size = int(sys.argv[2])
-    output_file = sys.argv[3]
+    min_scaf_size = int(sys.argv[3])
+    output_file = sys.argv[4]
 except:
     print(__doc__)
     sys.exit(1)
+
+# Normalize output by maximum
+# Generate 20 random sequences of window_size and find maximum_ratio
+print(f"Calibrating maximum entropy for windows of {window_size}bp")
+maximum_ratio = 0.0
+
+for i in range(20):
+    random_sequence = "".join(choice("ACGT") for _ in range(window_size))
+    ratio = compression_ratio(random_sequence)
+    print(ratio, end=" ", flush=True)
+    maximum_ratio = max(ratio, maximum_ratio)
+
+print()
 
 # Extract complexity by sliding window
 tot_pos = 0
@@ -75,7 +99,7 @@ with myopen(output_file, "wt") as outfile:
 
     # If scaffold doesn't have at least 2 full windows, skip
     for s in sequences:
-        if len(s.sequence) < (2 * window_size):
+        if len(s.sequence) < (2 * window_size) or len(s.sequence) < min_scaf_size:
             continue
 
         pos = 0
@@ -85,8 +109,8 @@ with myopen(output_file, "wt") as outfile:
             window, s.sequence = s.sequence[: window_size], s.sequence[window_size: ]
 
             # Write stats to output
-            compression_ratio = len(gzip.compress(window.upper().encode())) / len(window)
-            line = "\t".join([s.name, str(pos), str(tot_pos), str(round(compression_ratio, 6))])
+            line = "\t".join([s.name, str(pos), str(tot_pos),
+                str(round(compression_ratio(window, maximum_ratio), 6))])
 
             outfile.write(line + "\n")
             outfile.flush()
@@ -100,10 +124,10 @@ with myopen(output_file, "wt") as outfile:
         
         # Delimiter between chromosomes
         # Twix, definition:
-        #(a) With ref. to position or location in space: among (several
-        #animals); in among (surrounding objects); ~ hondes, in (one's) hands;
-        #(b) with ref. to association or relationship: between two
-        #(parties); also, among (parties) [quot. a1400, last]; ben born
+        #     (a) With ref. to position or location in space: among (several
+        #         animals); in among (surrounding objects); ~ hondes, in (one's) hands;
+        #     (b) with ref. to association or relationship: between two
+        #         (parties); also, among (parties) [quot. a1400, last];
         tot_pos -= window_size
         outfile.write(f"Twix\t{pos}\t{tot_pos}\t0\n")
         tot_pos += window_size * 5
