56a79c5c28bb8bb1dd936f01d078e5f6fac0355d
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-04-18 09:36:21 -0400

    Modernise fasta_extract_min_length.py

--- fasta_extract_min_length.py
+++ fasta_extract_min_length.py
@@ -1,32 +1,76 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/env python3
 """Extract fasta sequences that are above a minimal length.
 
 Usage:
-    %program <input_file> <min_length> <output_file>"""
+    <program> input_file min_length output_file
+"""
 
+# Modules
+import gzip
 import sys
-import re
+
+# Classes
+class Fasta(object):
+    """Fasta object with name and sequence
+    """
+
+    def __init__(self, name, sequence):
+        self.name = name
+        self.sequence = sequence
+
+    def write_to_file(self, handle):
+        handle.write(">" + self.name + "\n")
+        handle.write(self.sequence + "\n")
+
+    def __repr__(self):
+        return self.name + " " + self.sequence[:31]
+
+# Defining functions
+def myopen(_file, mode="rt"):
+    if _file.endswith(".gz"):
+        return gzip.open(_file, mode=mode)
+
+    else:
+        return open(_file, mode=mode)
+
+def fasta_iterator(input_file):
+    """Takes a fasta file input_file and returns a fasta iterator
+    """
+    with myopen(input_file) as f:
+        sequence = []
+        name = ""
+        begun = False
+
+        for line in f:
+            line = line.strip()
+
+            if line.startswith(">"):
+                if begun:
+                    yield Fasta(name, "".join(sequence))
+
+                name = line[1:]
+                sequence = ""
+                begun = True
+
+            else:
+                sequence += line
+
+        if name != "":
+            yield Fasta(name, "".join(sequence))
+
 
 try:
-    from Bio import SeqIO
-except:
-    print("This program requires the Biopython library")
-    sys.exit(0)
-
-try:
-    fasta_file = sys.argv[1]      # Input fasta file
+    input_file = sys.argv[1]      # Input fasta file
     min_length = int(sys.argv[2]) # Minimum length of sequence
-    result_file = sys.argv[3]     # Output fasta file
+    output_file = sys.argv[3]     # Output fasta file
 except:
     print(__doc__)
     sys.exit(0)
 
-fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+fasta_sequences = fasta_iterator(input_file)
 
-with open(result_file, "w") as f:
+with open(output_file, "wt") as outfile:
     for seq in fasta_sequences:
-        if len(str(seq.seq)) >= min_length:
-            SeqIO.write([seq], f, "fasta")
+        if len(seq.sequence) >= min_length:
+            seq.write_to_file(outfile)
 
