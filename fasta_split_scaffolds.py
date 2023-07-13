20e71290aebaa82a3f408bf3f48feb297134a81b
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-07-13 13:31:35 -0400

    Big update

--- fasta_split_scaffolds.py
+++ fasta_split_scaffolds.py
@@ -1,94 +1,100 @@
-#!/usr/bin/env python
-"""Split scaffolds by cutting in long stretches of Ns
+#!/usr/bin/env python3
+"""Split scaffolds at occurences of one or more Ns
 
 Usage:
-    fasta_split_scaffold.py infile min_len outfile
+    fasta_split_scaffold.py input_file min_Ns min_len output_file
 
-infile = fasta input file name
-min_len = minimum length of N stretch to cut
-outfile = fasta output file name
+input_file = fasta input file name
+min_Ns = minimum number of Ns to cut
+min_len = minimum sequence length to keep
+output_file = fasta output file name
 """
 
 # Modules
 import sys
 import re
 
+# Modules
+import gzip
+
 # Classes
 class Fasta(object):
     """Fasta object with name and sequence
     """
+
     def __init__(self, name, sequence):
         self.name = name
         self.sequence = sequence
+
     def write_to_file(self, handle):
         handle.write(">" + self.name + "\n")
         handle.write(self.sequence + "\n")
+
     def __repr__(self):
-        return(self.name + ": " + self.sequence[0:31])
+        return self.name + " (len: " + str(len(self.sequence)) + ") " + self.sequence[:31]
 
 # Functions
+def myopen(_file, mode="rt"):
+    if _file.endswith(".gz"):
+        return gzip.open(_file, mode=mode)
+
+    else:
+        return open(_file, mode=mode)
+
 def fasta_iterator(input_file):
     """Takes a fasta file input_file and returns a fasta iterator
     """
-    with open(input_file) as f:
-        sequence = ""
+    with myopen(input_file) as f:
+        sequence = []
         name = ""
         begun = False
+
         for line in f:
             line = line.strip()
+
             if line.startswith(">"):
                 if begun:
-                    yield Fasta(name, sequence)
-                name = line.replace(">", "")
+                    yield Fasta(name, "".join(sequence))
+
+                name = line[1:]
                 sequence = ""
                 begun = True
+
             else:
                 sequence += line
 
         if name != "":
-            yield Fasta(name, sequence)
+            yield Fasta(name, "".join(sequence))
 
-def split_sequence(sequence, min_len):
-    """Takes a Fasta object and split on N repeats of min_len or more
+def split_and_write_sequences(seq, min_Ns, min_len, output_file):
+    short_name = seq.name.split(" ")[0]
+    seq.name = short_name
+    fragments = list(re.finditer("[^nN]{" + str(min_len) + ",}", seq.sequence))
+    n = 0
 
-    Returns a list of Fasta objects
-    """
-    seq = sequence.sequence
-    name = sequence.name
+    for f in fragments:
+        n += 1
+        nstr = str(n).rjust(8, "0")
+        _from, _to = f.span()
+        print(short_name, _from, _to)
 
-    n_stretches = sorted(list(re.findall("N{" + str(min_len) + ",}", seq)),
-            cmp = lambda x,y: cmp(len(x), len(y)), reverse=True)
-
-    for s in n_stretches:
-        seq = seq.replace(s, "_SPLIT-HERE_")
-
-    sequences = []
-    sections = seq.split("_SPLIT-HERE_")
-
-    counter = 1
-    for s in sections:
-        sequences.append(Fasta(name + "_split_" + str(counter), s))
-        counter += 1
-
-    return sequences
+        if (_to - _from) > min_len:
+            Fasta("_".join([short_name, nstr]), seq.sequence[_from: _to]).write_to_file(outfile)
 
 # Main
 if __name__ == '__main__':
     try:
-        infile = sys.argv[1]
-        min_len = str(sys.argv[2])
-        outfile = sys.argv[3]
+        input_file = sys.argv[1]
+        min_Ns = int(sys.argv[2])
+        min_len = int(sys.argv[3])
+        output_file = sys.argv[4]
     except:
-        print __doc__
+        print(__doc__)
         sys.exit(1)
 
-    fasta_sequences = fasta_iterator(infile)
+    fasta_sequences = fasta_iterator(input_file)
     output = []
 
-    for seq in fasta_sequences:
-        output.extend(split_sequence(seq, min_len))
-    
-    with open(outfile, "w") as outf:
-        for seq in output:
-            seq.write_to_file(outf)
-
+    with open(output_file, "w") as outfile:
+        for seq in fasta_sequences:
+            split_and_write_sequences(seq, min_Ns, min_len, outfile)
