b73c1a47e53e1f11e4c48755f9995bdac576dd03
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-07-28 09:09:46 -0400

    Convert to 1000x1000 px

--- 00_deprecated/convert_boto.sh
+++ 00_deprecated/convert_boto.sh
@@ -7,6 +7,6 @@ time for file in $(find $FOLDER -iname "*.jpg" | grep -v _small)
 do
     echo $file
     NEWNAME=$(echo $file | perl -pe 's/(\.jpg|\.JPG)/_small\1/')
-    convert -resize 800x800 -unsharp 1.0x1.0+1+0.04 $file $NEWNAME
+    convert -resize 1000x1000 -unsharp 1.0x1.0+1+0.04 $file $NEWNAME
 done
 
