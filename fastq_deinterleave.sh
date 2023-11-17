69acc97b1d1d29433c1dc67af2ddb631885885fc
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-11-17 14:50:39 -0500

    Add fastq_deinterleave.sh

---
+++ fastq_deinterleave.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Deinterleave fastq files assuming each pair of sequences has 8 lines
+
+# Usage:
+#  <program> input_fastq output1 output2
+
+gunzip -c $1 | paste - - - - | tee >(cut -f 1-4 | tr "\t" "\n" > $2) | cut -f 5-8 | tr "\t" "\n" > $3
