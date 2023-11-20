ce30cf692136bd1642c57329d222972fb0a83871
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-11-20 09:01:29 -0500

    Produce gziped output

--- fastq_deinterleave.sh
+++ fastq_deinterleave.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Deinterleave fastq files assuming each pair of sequences has 8 lines
+# Assumes the input is compressed with gzip and produces two gzip-compressed files
 
 # Usage:
-#  <program> input_fastq output1 output2
+#  <program> input_fastq output_forward output_reverse
 
-gunzip -c $1 | paste - - - - - - - - | tee >(cut -f 1-4 | tr "\t" "\n" > $2) | cut -f 5-8 | tr "\t" "\n" > $3
+gunzip -c $1 |
+    paste - - - - - - - - |
+    tee >(cut -f 1-4 | tr "\t" "\n" | gzip > $2) |
+    cut -f 5-8 | tr "\t" "\n" | gzip > $3
