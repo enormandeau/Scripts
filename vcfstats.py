9268d0281291a012fc35d57af6d12552c1367529
Eric Normandeau <eric.normandeau.qc@gmail.com>
2024-11-28 11:25:48 -0500

    Update vcfstats.py

--- vcfstats.py
+++ vcfstats.py
@@ -25,52 +25,51 @@ except:
     print(__doc__)
     sys.exit(1)
 
-# Report on:
-# --
-# VCF name (input_vcf)
-# STACKS version (version)
-# File date (date)
-# Number of samples (num_samples)
-# Number of SNPs
-# Proportion of missing
-# Median coverage
-
 # Extract stats
 num_geno = 0
 num_miss = 0
 num_snps = 0
+loci = set()
 coverages = []
 
 print(f"{input_vcf.split('/')[-1]}")
 print("--")
 
 with myopen(input_vcf, "rt") as infile:
-    for line in infile:
-        l = line.strip().split("\t")
+    try:
+        for line in infile:
+            l = line.strip().split("\t")
 
-        if not line.startswith("#"):
-            data = l[9:]
-            num_snps += 1
-            num_geno += num_samples
-            num_miss += len([x for x in data if "./." in x])
-            coverages += [int(x.split(":")[1]) for x in data if x != "./." and x.split(":")[1] != "0"]
+            if not line.startswith("#"):
+                locus = l[2].split(":")[0]
+                loci.add(locus)
+                num_snps += 1
+                num_geno += num_samples
 
-        elif "##fileDate=" in line:
-            date = l[0].split("=")[1]
-            date = date[:4] + "-" + date[4: 6] + "-" + date[6:]
-            print(f"Date: {date}")
+                data = l[9:]
+                num_miss += len([x for x in data if "./." in x])
+                coverages += [int(x.split(":")[1]) for x in data if x != "./." and x.split(":")[1] != "0"]
 
-        elif "##source=" in line:
-            version = l[0].split("=")[1].replace('"', '')
-            print(f"Version: {version}")
-        
-        elif l[0] == "#CHROM":
-            num_samples = len(l[9:])
-            num_populations = len(set([x.split("_")[0] for x in l[9:]]))
-            print(f"Samples: {num_samples}")
-            print(f"Populations: {num_populations}")
+            elif "##fileDate=" in line:
+                date = l[0].split("=")[1]
+                date = date[:4] + "-" + date[4: 6] + "-" + date[6:]
+                print(f"Date: {date}")
+
+            elif "##source=" in line:
+                version = l[0].split("=")[1].replace('"', '')
+                print(f"Version: {version}")
+
+            elif l[0] == "#CHROM":
+                num_samples = len(l[9:])
+                num_populations = len(set([x.split("_")[0] for x in l[9:]]))
+                print(f"Samples: {num_samples}")
+                print(f"Populations: {num_populations}")
+
+    except EOFError:
+        print("\n  !! WARNING:\n  !! Results below are from an incomplete compressed VCF file\n")
 
 print(f"SNPs: {num_snps}")
+print(f"loci: {len(loci)}")
 print(f"Missing: {round(100 * num_miss / num_geno, 2)}%")
 print(f"Coverage mean: {round(statistics.mean(coverages), 2)}")
 print(f"Coverage median: {statistics.median(coverages)}")
